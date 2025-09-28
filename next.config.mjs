aef9a546894d1dc5ae5c996813ce9a144d4fd228
Jason Desiderio <jason@thegoodfornothings.club>
2025-09-28 18:55:15 -0500

    Update next to latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  },
+  typedRoutes: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com"
-      }
-    ]
-  }
-};
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
