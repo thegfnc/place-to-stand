45241fe6e7977982c7c88498d5904d7143079289
Jason Desiderio <jason@thegoodfornothings.club>
2025-09-28 19:06:44 -0500

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,17 @@
-import nextConfig from "eslint-config-next";
+import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
-const config = [
-  ...nextConfig,
-  {
-    rules: {
-      "react/no-unescaped-entities": "off"
-    }
-  }
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-export default config;
+export default defineConfig([{
+    extends: compat.extends("next/core-web-vitals", "next/typescript"),
+}]);
